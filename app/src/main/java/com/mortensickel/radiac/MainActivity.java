460d67ee839c9e3b45c9d9004876cb3037500426
Morten Sickel <morten@sickel.net>
2015-03-16 08:35:59 +0100

    enabling and disabling elements

--- app/src/main/java/com/mortensickel/radiac/MainActivity.java
+++ app/src/main/java/com/mortensickel/radiac/MainActivity.java
@@ -3,10 +3,19 @@ package com.mortensickel.radiac;
 import android.app.*;
 import android.os.*;
 import android.view.*;
+import android.widget.Button;
+import android.widget.*;
+import java.util.List;
+import java.util.*;
+import java.text.*;
 
 public class MainActivity extends Activity 
 {
     @Override
+	
+private Calendar startTime,stopTime;
+private SimpleDateFormat sdtHhmmss = new SimpleDateFormat("HH:mm:ss");	
+	
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
@@ -15,5 +24,52 @@ public class MainActivity extends Activity
 	
 	public void onMeastypeClicked(View v){
 		
+	}
+	
+	public void onMeasureStart(View v){
+		Button bt=(Button)findViewById(R.id.btStartMeasure);
+		bt.setEnabled(false);
+		List<Integer> toEnable=Arrays.asList(R.id.btStopMeasure,R.id.etAdmname,R.id.etComment,R.id.etLocname,R.id.etSnowcover
+		,R.id.cbReference,R.id.cbOtherMeasure,R.id.cbRainBefore,R.id.cbRainDuring);
+		for (Integer i : toEnable){
+			View et=findViewById(i);
+			et.setEnabled(true);
+		}
+		startTime=Calendar.getInstance();
+		EditText st=(EditText)findViewById(R.id.etTimeFrom);
+		st.setText(sdtHhmmss.format(startTime.getTime()));
+	}
+	
+	
+	public void onMeasureStop(View v){
+		Button bt=(Button)findViewById(R.id.btStopMeasure);
+		bt.setEnabled(false);
+		List<Integer> toEnable=Arrays.asList(R.id.etMeasValue,R.id.etLatitude,R.id.etLongitude,R.id.etTimeFrom,R.id.etTimeTo);
+		for (Integer i : toEnable){
+			View et=findViewById(i);
+			et.setEnabled(true);
+		}
+		stopTime=Calendar.getInstance();
+		EditText st=(EditText)findViewById(R.id.etTimeTo);
+		st.setText(sdtHhmmss.format(stopTime.getTime()));
+	}
+	
+	private void debug(String t){
+		Toast.makeText(getApplicationContext(),t,Toast.LENGTH_SHORT).show();
+	}
+	
+	
+	
+	public void checkFilled(View v){
+		List<Integer> mandatory=Arrays.asList(R.id.etAdmname,R.id.etLatitude,R.id.etLocname,R.id.etLongitude,R.id.etMeasValue,R.id.etSnowcover,R.id.etTimeFrom,R.id.etTimeTo);
+		Boolean ready=true;
+	//	debug("in");
+		for(Integer i:mandatory){
+			EditText et=(EditText)findViewById(i);
+			ready=ready && !(et.getText().toString().isEmpty());
+		}
+//		debug("out");
+		View save=findViewById(R.id.btConfirm);
+		save.setEnabled(ready);
 	}
 }
